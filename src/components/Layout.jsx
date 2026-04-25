464cc28646145c82ffbf59eb8a352483f43cd69d
Shishir Timalsina <shishir.01@outlook.com>
2026-04-25 19:34:51 +1000

    added brand favicon

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,6 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import GetStartedModal from "./GetStartedModal";
